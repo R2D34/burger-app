485baf59ab08ffb24971705f9f3ed8de337daf4f
Neuroblade <alx@arch.pc.localdomain>
2020-08-06 12:11:17 +0200

    Connected the more button to ingredients state

--- burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-app/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,7 +6,9 @@ const buildControl = (props) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Less</button>
-    <button className={console.More}>More</button>
+    <button className={classes.More} onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
