a6be6ededd605090716fa83ed306b4b1a737ab26
Neuroblade <alx@arch.pc.localdomain>
2020-08-24 13:36:46 +0200

    Removing the unneeded interceptor for ErrorHandler

--- burger-app/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger-app/src/hoc/withErrorHandler/withErrorHandler.js
@@ -12,12 +12,12 @@ const withErrorHandler = (WrappedComponent, axios) => {
     constructor() {
       super();
       //First clear error with each send request which might have stayed from previous code executions
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
       });
 
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState({ error: error });
@@ -28,6 +28,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
     errorConfirmedHandler = () => {
       this.setState({ error: null });
     };
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
+    }
 
     render() {
       return (
