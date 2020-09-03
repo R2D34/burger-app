da5de9d4763ccb36bf19d493dec56a9e74fc960c
Neuroblade <alx@arch.pc.localdomain>
2020-09-03 14:34:14 +0200

    Hooked buttons in CheckoutSummary to routes

--- burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,29 +91,30 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    // alert("You continue!");
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      cusmoter: {
-        name: "Son Goku",
-        address: {
-          street: "Golden Valley 2",
-          zipCode: "12-2111",
-          country: "Japn",
-        },
-        email: "testing@testicles.com",
-      },
-    };
-    axios
-      .post("/orders.json", order)
-      .then((response) => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch((error) => {
-        this.setState({ loading: false, purchasing: false });
-      });
+    // // alert("You continue!");
+    // this.setState({ loading: true });
+    // const order = {
+    //   ingredients: this.state.ingredients,
+    //   price: this.state.totalPrice,
+    //   cusmoter: {
+    //     name: "Son Goku",
+    //     address: {
+    //       street: "Golden Valley 2",
+    //       zipCode: "12-2111",
+    //       country: "Japn",
+    //     },
+    //     email: "testing@testicles.com",
+    //   },
+    // };
+    // axios
+    //   .post("/orders.json", order)
+    //   .then((response) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   })
+    //   .catch((error) => {
+    //     this.setState({ loading: false, purchasing: false });
+    //   });
+    this.props.history.push("/checkout");
   };
 
   render() {
