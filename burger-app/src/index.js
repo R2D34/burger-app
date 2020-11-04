ad140a1ecc772ac26ad0d83f60a4bba57a985d48
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-04 14:28:52 +0100

    Added saga and initiated it

--- burger-app/src/index.js
+++ burger-app/src/index.js
@@ -5,10 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga';
 
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+
+import {logoutSaga} from "./store/sagas/auth"
 
 import "./index.css";
 import App from "./App";
@@ -20,13 +23,20 @@ const composeEnhancers =
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null || compose;
 
-const rootReucer = combineReducers({
+const rootReducer = combineReducers({
   burgerBuilder: burgerBuilderReducer,
   order: orderReducer,
   auth: authReducer,
 });
 
-const store = createStore(rootReucer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
+);
+
+sagaMiddleware.run(logoutSaga);
 
 const app = (
   <Provider store={store}>
