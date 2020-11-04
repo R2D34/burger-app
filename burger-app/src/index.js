d0fbc956fb25b78b210691aac7944cbad8e142d4
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-04 14:40:03 +0100

    Dispatched new action through saga

--- burger-app/src/index.js
+++ burger-app/src/index.js
@@ -11,7 +11,7 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 
-import {logoutSaga} from "./store/sagas/auth"
+import {watchAuth} from "./store/sagas/index"
 
 import "./index.css";
 import App from "./App";
@@ -36,7 +36,7 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
