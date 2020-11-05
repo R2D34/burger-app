dc21ad50e870bfb78881b6436ebb86887a8a76be
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-05 17:48:56 +0100

    Added saga from checkAuthTimeout

--- burger-app/src/store/sagas/index.js
+++ burger-app/src/store/sagas/index.js
@@ -1,10 +1,11 @@
 import {takeEvery} from 'redux-saga/effects'
 
-import {logoutSaga} from './auth';
+import {logoutSaga, checkAuthTimeoutSaga } from './auth';
 
 
 import * as actionTypes from "../actions/actionsTypes"
 
 export function* watchAuth (){
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga)
 }
