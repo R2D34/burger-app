b119eae77e69c7d1eb9054d1db6c5a05aab9fd34
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-10 09:58:27 +0100

    Moved authUser to authUserSaga

--- burger-app/src/store/sagas/auth.js
+++ burger-app/src/store/sagas/auth.js
@@ -2,6 +2,7 @@ import {delay} from 'redux-saga/effects'
 import { put } from "redux-saga/effects";
 
 import * as actions from "../actions/index";
+import axios from 'axios';
 
 export function* logoutSaga(action) {
   yield localStorage.removeItem("token");
@@ -16,3 +17,40 @@ export function* checkAuthTimeoutSaga(action) {
   yield put(actions.logout())
   
 }
+
+export function* authUserSaga(action) {
+yield put(actions.authStart());
+    const authData = {
+      email: action.email,
+      password: action.password,
+      returnSecureToken: true,
+    };
+    let url =
+      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + process.env.REACT_APP_FIREBASE_API_KEY;
+
+    if (!action.isSignup) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" + process.env.REACT_APP_FIREBASE_API_KEY;
+    }
+    try {
+      const response = yield axios.post(url, authData)
+    axios
+      .post(url, authData)
+ 
+
+        const expirationDate = yield new Date(
+          new Date().getTime() + response.data.expiresIn * 1000
+        );
+
+        yield localStorage.setItem("token", response.data.idToken);
+        yield localStorage.setItem("expirationDate", expirationDate);
+        yield localStorage.setItem("userId", response.data.localId);
+
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId));
+        yield put(actions.checkAuthTimeout(response.data.expiresIn));
+   
+    } catch(error){
+      yield put(actions.authFail(error.response.data.error))
+    }
+    
+  };
