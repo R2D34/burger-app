1300a16bbcb5712882467d8bc6c5feae21411eb9
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-10 10:50:18 +0100

    finished moving auth sideeffects to sagas

--- burger-app/src/store/actions/index.js
+++ burger-app/src/store/actions/index.js
@@ -6,4 +6,4 @@ export {
 
 export { purchaseBurger, purchaseInit, fetchOrders } from "./order";
 
-export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, checkAuthTimeout, authFail } from "./auth";
+export { auth, logout, setAuthRedirectPath, authCheckState, logoutSucceed, authStart, authSuccess, checkAuthTimeout, authFail,  } from "./auth";
