b6f75f95a0473b4ba8dc2dac1f2f8f9549c23be3
Neuroblade <9194961+Tsubanee@users.noreply.github.com>
2020-11-21 17:33:20 +0100

    Using React.lazy instead of custom async comp

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -3,20 +3,19 @@ import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import Layout from "./hoc/Layout/Layout";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
 
-const asyncCheckout = asyncComponent(() => {
+const asyncCheckout = React.lazy(() => {
   return import("./containers/Checkout/Checkout");
 });
-const asyncOrders = asyncComponent(() => {
+const asyncOrders = React.lazy(() => {
   return import("./containers/Orders/Orders");
 });
-const asyncAuth = asyncComponent(() => {
+const asyncAuth = React.lazy(() => {
   return import("./containers/Auth/Auth");
 });
 
